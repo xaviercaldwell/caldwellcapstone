cf5dbf178bcda4ea19e883f1571ce251064cf208
xaviercaldwell <xvrianreed@gmail.com>
2025-12-04 19:09:14 -0700

    Revert "please"
    This reverts commit 2054928970563127988b93f4572e732840e41742.

--- proxy.ts
+++ proxy.ts
@@ -1,13 +1,14 @@
-import { NextRequest, NextResponse } from 'next/server'
- 
-export function proxy(request: NextRequest) {
-  const nonce = Buffer.from(crypto.randomUUID()).toString('base64')
-    const isDev = process.env.NODE_ENV === 'development'
+import { NextResponse } from "next/server";
+
+export function proxy(request: Request) {
+  const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
 
   const cspHeader = `
     default-src 'self';
-    script-src 'self' 'nonce-${nonce}' 'strict-dynamic' ${isDev ? "'unsafe-eval'" : ''};
-    style-src 'self' ${isDev ? "'unsafe-inline'" : `'nonce-${nonce}'`};
+    script-src 'self' 'nonce-${nonce}' 'strict-dynamic' https: http: ${
+      process.env.NODE_ENV === "production" ? "" : `'unsafe-eval'`
+    };
+    style-src 'self' 'nonce-${nonce}';
     img-src 'self' blob: data:;
     font-src 'self';
     object-src 'none';
@@ -15,29 +16,27 @@ export function proxy(request: NextRequest) {
     form-action 'self';
     frame-ancestors 'none';
     upgrade-insecure-requests;
-`
-  // Replace newline characters and spaces
-  const contentSecurityPolicyHeaderValue = cspHeader
-    .replace(/\s{2,}/g, ' ')
-    .trim()
- 
-  const requestHeaders = new Headers(request.headers)
-  requestHeaders.set('x-nonce', nonce)
- 
-  requestHeaders.set(
-    'Content-Security-Policy',
-    contentSecurityPolicyHeaderValue
-  )
- 
-  const response = NextResponse.next({
-    request: {
-      headers: requestHeaders,
-    },
-  })
-  response.headers.set(
-    'Content-Security-Policy',
-    contentSecurityPolicyHeaderValue
-  )
- 
-  return response
+  `;
+
+  const contentSecurityPolicy = cspHeader
+    .replace(/\s{2,}/g, " ")
+    .trim();
+
+  const res = NextResponse.next();
+  res.headers.set("Content-Security-Policy", contentSecurityPolicy);
+  res.headers.set("x-nonce", nonce);
+
+  return res;
 }
+
+export const config = {
+  matcher: [
+    {
+      source: "/((?!api|_next/static|_next/image|favicon.ico).*)",
+      missing: [
+        { type: "header", key: "next-router-prefetch" },
+        { type: "header", key: "purpose", value: "prefetch" },
+      ],
+    },
+  ],
+};
